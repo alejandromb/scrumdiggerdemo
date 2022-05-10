b58ab15da9b1220af7c338cef3c93707253703c1
Alejandro Mena <alejandrobrito88@gmail.com>
2022-05-10 13:59:47 -0400

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Alejandro Mena Brito on 5/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
